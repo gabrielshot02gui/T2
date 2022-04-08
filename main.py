5cfc8d70a8b06769be8c5c60f7941f5aefb104cf
Tech_Rop <74739219+gabrielshot02gui@users.noreply.github.com>
2022-04-08 16:31:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-nome = input("Qual o seu nome: ")
-num1 = int(input("Quanto você tirou na primeira prova: "))
-num2 = int(input("Quanto você tirou na segunda prova: "))
+name = input("Whats your name: ")
+num1 = int(input("How much did you get on the first test: ")) #use numbers
+num2 = int(input("How much did you get on the second test: ")) #use numbers here too
 total = num1 + num2 
-media = total/2
-ps = media<5
-if ps == True:
-  print("Tente melhorar suas notas")
+average = total/2
+yp = average<5
+if yp == True:
+  print("Try to improve your grades")
 else:
-  print("Parabéns",nome,"você passou")
+  print("Congratulations",name,"you passed")
+  
